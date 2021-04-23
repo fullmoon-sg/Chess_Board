69761393a0d06681cc130feedefdb18a767c731b
fullmoon-sg <mbfoo7757@gmail.com>
2021-04-23 14:40:25 +0000

    Done initial board

---
+++ chess/src/components/Board.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../css/board.css'
+
+export default class Board extends React.Component {
+
+    render(){
+        let board = [];
+        for (let i=0; i < 19 ; i++){
+            for (let j = 0; j < 19; j++){
+                board.push(<div className="board-cell">{i}{j}</div>)
+            }
+        }
+     
+    return (
+        <div className="main-board">
+        <div className="board-size">
+           {board}
+        </div>
+        </div>
+    )
+}
+}
+
